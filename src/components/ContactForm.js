59d5eff8dd127e33e3238cd600e0c835ca3395ed
cameronduffin <camduffin@gmail.com>
2021-05-18 18:55:33 -0400

    cleaning up contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -5,11 +5,13 @@ import * as contactStyles from './contact.module.scss'
 function ContactForm() {
     const [state, handleSubmit] = useForm("form");
     if (state.succeeded) {
-        return <p className="submit-message">We'll be in touch shortly.</p>;
+        return <p>We'll be in touch shortly.</p>;
     }
     return (
 
         <form onSubmit={handleSubmit} className={contactStyles.form}>
+            <label htmlFor="name" className="sr-only">name</label>
+            <input type="text" id="name" name="name" placeholder="name" required/>
             <label htmlFor="email" className="sr-only">email</label>
             <input id="email" type="email" name="email" placeholder="email" required />
             <ValidationError prefix="Email" field="email" errors={state.errors} />
@@ -17,8 +19,6 @@ function ContactForm() {
             <textarea id="message" name="message" placeholder="message" required/>
             <ValidationError prefix="Message" field="message" errors={state.errors} />
 
-            <label htmlFor="name" className="sr-only">name</label>
-            <input type="text" id="name" name="name" placeholder="name" required/>
 
             <button className={contactStyles.button} type="submit" disabled={state.submitting}>
             send
