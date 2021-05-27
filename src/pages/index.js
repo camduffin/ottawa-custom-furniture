2eb115262142b9ca8cd7f7fc952daef4e3846df3
cameronduffin <camduffin@gmail.com>
2021-05-27 15:43:32 -0400

    coupla changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,28 +5,32 @@ import Seo from "../components/Seo"
 import Contact from '../components/Contact'
 
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <section className={homeStyles.headerBG}>
-      <div className={homeStyles.wrapper}>
-        <div className={homeStyles.mainHeader}>
-          <div className={homeStyles.mainHeaderText}>
-            <h1 className={homeStyles.h1}>Thoughtfully designed, beautiful & functional.</h1>
-          </div>
-          <a href="#contact">
-            <div class={homeStyles.arrow}>
+const IndexPage = () => {
+
+  return (
+        
+        <Layout>
+          <Seo title="Home" />
+          <section className={homeStyles.headerBG}>
+            <div className={homeStyles.wrapper}>
+              <div className={homeStyles.mainHeader}>
+                <div className={homeStyles.mainHeaderText}>
+                  <h1 className={homeStyles.h1}>Thoughtfully designed, beautiful & functional.</h1>
+                </div>
+                <a href="#contact">
+                  <div class={homeStyles.arrow}>
                     <span></span>
                     <span></span>
                     <span></span>
                     down arrow
+                  </div>
+              </a>
+              </div>
             </div>
-        </a>
-        </div>
-      </div>
-    </section>
-    <Contact />
-  </Layout>
-)
+          </section>
+          <Contact />
+        </Layout>
+  )
+}
 
-export default IndexPage
+export default IndexPage;
