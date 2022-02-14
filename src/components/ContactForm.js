cc19505d1fb21164c65803911fc14f30d3649c36
Cam Duffin <camduffin@gmail.com>
2022-02-13 17:02:05 -0800

    new labels for the form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -32,16 +32,16 @@ function ContactForm() {
 
         <form name="Quote" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" className={quoteStyles.form}>
             <input type="hidden" name="form-name" value="Quote" />
-            <label htmlFor="name" className="sr-only">name</label>
+            <label htmlFor="name" className="sr-only">Name</label>
             <input type="text" id="name" name="name" placeholder="Name" required/>
 
-            <label htmlFor="email" className="sr-only">email</label>
+            <label htmlFor="email" className="sr-only">Email</label>
             <input id="email" type="email" name="email" placeholder="Email" required />
 
-            <label htmlFor="phone" className="sr-only">name</label>
+            <label htmlFor="phone" className="sr-only">Phone Number</label>
             <input type="text" id="phone-number" name="phone-number" placeholder="Phone #" required/>
 
-            <label htmlFor="message" className="sr-only">message</label>
+            <label htmlFor="message" className="sr-only">Message</label>
             <textarea id="message" name="message" placeholder="Type your comments about the project..." required/>
 
             <button type="submit" className={quoteStyles.button}>Submit</button>
