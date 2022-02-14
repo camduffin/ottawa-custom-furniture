d5b21e707edfbb2a65de39c7e743a2e1f4544bcd
Cam Duffin <camduffin@gmail.com>
2022-02-13 16:53:41 -0800

    fixing form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -32,17 +32,14 @@ function ContactForm() {
 
         <form name="Quote" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" className={quoteStyles.form}>
             <input type="hidden" name="form-name" value="Quote" />
-            <label htmlFor="first-name" className="sr-only">name</label>
-            <input type="text" id="first-name" name="first-name" placeholder="First Name" required/>
-
-            <label htmlFor="last-name" className="sr-only">name</label>
-            <input type="text" id="last-name" name="last-name" placeholder="Last Name" required/>
+            <label htmlFor="name" className="sr-only">name</label>
+            <input type="text" id="name" name="name" placeholder="Name" required/>
 
             <label htmlFor="email" className="sr-only">email</label>
             <input id="email" type="email" name="email" placeholder="Email" required />
 
             <label htmlFor="phone" className="sr-only">name</label>
-            <input type="text" id="phone" name="phone" placeholder="Phone #" required/>
+            <input type="text" id="phone-number" name="phone-number" placeholder="Phone #" required/>
 
             <label htmlFor="message" className="sr-only">message</label>
             <textarea id="message" name="message" placeholder="Type your comments about the project..." required/>
