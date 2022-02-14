dccfe62d951c5cddf28e313ed0bccb0afcc609a0
Cam Duffin <camduffin@gmail.com>
2022-02-14 15:11:59 -0800

    removing unused pages and components

--- src/pages/about.js
+++ src/pages/about.js
@@ -23,6 +23,9 @@ const AboutPage = () => {
                         biographyTextTwo {
                             biographyTextTwo
                         }
+                        biographyTextThree {
+                            biographyTextThree
+                        }
                     }
                 }
             }
@@ -53,10 +56,12 @@ const AboutPage = () => {
                                 <p>{edge.node.biographyTextTwo.biographyTextTwo}</p>
                             )
                         })}
-                            {/* <p>Ottawa Custom Furniture is the work of Murray Spencer, he's a really great guy and you know what? I think you'll really like him. He prides himself on doing a good job every job no exceptions. What the hell does that mean you ask? No one can really be quite sure. Murray was dubbed 'king wood' by Food & Drink magazine in 2020 due to his unreasonably large desire to cook up some delicious tables, chairs, and tables with chairs. Tableschairs.</p> 
-                                
-                            
-                            <p>Give Murray a shot and he might just deliver. Next time you need to have a woodworker build your tables or stools I think between you and me we both know you're going to choose this guy to do the job right and only once. That's our motto. <span>Right and only once.</span></p> */}
+
+                        {data.allContentfulAbout.edges.map((edge) => {
+                            return (
+                                <p>{edge.node.biographyTextThree.biographyTextThree}</p>
+                            )
+                        })}
                             <AniLink to="/quote">
                                 <button className={aboutStyles.button}>Get in Touch</button>
                             </AniLink>
