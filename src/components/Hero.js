7b803514baeb691fcc9c43d8ea7b73d6d23a2883
Cam Duffin <camduffin@gmail.com>
2022-03-03 16:29:12 -0800

    adjust window target and add parrallax back in

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,43 +3,48 @@ import * as homeStyles from './home.module.scss'
 import image from '../images/conferenceHeader.jpg'
 import { StaticImage } from 'gatsby-plugin-image'
 import { Parallax } from 'react-parallax';
-import { useState, useEffect } from "react"
+// import { useState, useEffect } from "react"
 
 const HeroImage = () => {
 
     const mobileImage = '../images/conferenceHeader.jpg'
 
-    const [width, setWidth] = useState(window.innerWidth);
-    const breakpoint = 1100;
+    // const [width, setWidth] = useState(window.innerWidth);
+    // const breakpoint = 1100;
 
-    useEffect(() => {
-        const handleResizeWindow = () => setWidth(window.innerWidth);
+    // useEffect(() => {
+    //     const handleResizeWindow = () => setWidth(window.innerWidth);
     
-        window.addEventListener("resize", handleResizeWindow);
+    //     window.addEventListener("resize", handleResizeWindow);
         
-        return () => {
+    //     return () => {
     
-            window.removeEventListener("resize", handleResizeWindow);
-        };
-    }, []);
+    //         window.removeEventListener("resize", handleResizeWindow);
+    //     };
+    // }, []);
 
-    if (width > breakpoint) {
+    // if (width > breakpoint) {
+    //     return (
+    //         <Parallax className={homeStyles.paraHeader} 
+    //             bgImage={image} strength={400} 
+    //             bgImageAlt="A wooden conference room table with blue chairs around it"
+    //             >
+    //         </Parallax>
+    //     );
+    //     }
         return (
+            // <StaticImage className={homeStyles.paraHeader} 
+            //     src={mobileImage} 
+            //     alt="A wooden conference room table with blue chairs around it"
+            //     width={1500}
+            //     height={1000}
+            //     >
+            // </StaticImage>
             <Parallax className={homeStyles.paraHeader} 
                 bgImage={image} strength={400} 
                 bgImageAlt="A wooden conference room table with blue chairs around it"
                 >
             </Parallax>
-        );
-        }
-        return (
-            <StaticImage className={homeStyles.paraHeader} 
-                src={mobileImage} 
-                alt="A wooden conference room table with blue chairs around it"
-                width={1500}
-                height={1000}
-                >
-            </StaticImage>
             // <div className={homeStyles.parallax}></div>
         );
 }
